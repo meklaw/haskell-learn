c0ed8b7646a4be67070a8eb23cf41295e55cc246
Nikita Ulyanov <nik.ule@yandex.ru>
2023-08-20 01:26:55 +0300

    make 1 module

--- introduction/functions/HelloModule.hs
+++ introduction/functions/HelloModule.hs
@@ -1,3 +1,5 @@
+module HelloModule where
+
 -- ghci - запуск командного интерпретатора в командной строке
 -- ghci Hello.hs - запустит интерпретатор и загрузит в него модуль
 -- ghci ./src/Hello.hs - запустит интерпретатор и загрузит в него модуль
@@ -8,3 +10,21 @@
 -- :reload TestModule - перезагрузит модуль 
 
 main = putStrLn "Hello, world!"
+
+sayHello = putStrLn "Hello World frome modul Test!"
+
+sumSquares x y = x^2 + y^2
+
+funcA x = if x < 0 then -1 else 1
+
+funcB x = (if x < 0 then -1 else 1) + 10
+
+funcC x = if mod x 2 == 0 then "Nice" else "faq"
+
+sign x = if x > 0 then 1 else if x == 0 then 0 else -1
+
+-- Частичная функция, ей можно передать аргумент max10 16
+
+max10 = max 10 
+
+max10' x = max 10 x
